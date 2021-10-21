49251dcd03cf7627be9accad79e9ec90dd7a0eec
aanhlle <levietanh1112@gmail.com>
2021-10-21 16:51:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/reworksample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.reworksample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.reworksample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
